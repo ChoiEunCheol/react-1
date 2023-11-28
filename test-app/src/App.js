7a28d5e36439be141f2b4d8758d3e0accdc38eec
최은철 <wndqhrgkdlel@gmail.com>
2023-11-28 15:38:45 +0900

    .

---
+++ test-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Test from './Test';
+
+class App extends Component {
+  render() {
+    return (
+    	<h1>Hello World!</h1>
+        <Test />
+    );
+  }
+}
+
+export default App;
