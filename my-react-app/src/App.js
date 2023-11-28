e5ab8ece554fdd2683041c6add2af291d4531aa7
최은철 <wndqhrgkdlel@gmail.com>
2023-11-28 16:54:51 +0900

    라우팅 설정하기

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import './App.css';
-import HelloWorld from './components/HelloWorld';
-import Counter from './components/Counter';
-import NameForm from './components/NameForm';
-import DataFetching from './components/DataFetching';
+import React from "react";
+import "./App.css";
 
-function App() {
+// import NameForm from './components/NameForm';
+// import DataFetching from './components/DataFetching';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// function App() {
+//   return (
+//     <div className = "App">
+//       <header className='App-header'>
+//         <HelloWorld />
+//         <Counter />
+//         <NameForm />
+//         <DataFetching />
+//       </header>
+//     </div>
+//   )
+// }
+
+const App = () => {
   return (
-    <div className = "App">
-      <header className='App-header'>
-        <HelloWorld />
-        <Counter />
-        <NameForm />
-        <DataFetching />
-      </header>
-    </div>
-  )
-}
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <hr />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ my-react-app/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HelloWorld from "../components/HelloWorld";
+import Counter from "../components/Counter";
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloWorld />
+        <Counter />
+      </header>
+    </div>
+  );
+};
+
+export default Home;
